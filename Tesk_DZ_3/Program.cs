9b29f351f4c0d149ddb9c10e11701c70a1fc9bca
unknown <artem.popov.97@mail.ru>
2022-06-19 01:45:54 +0300

    tesk 3

---
+++ Tesk_DZ_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу , которая принимает на вход число (N)    3-> 1,8,27
+// и выдает таблицу кубов чисел от 1 до N.                     5-> 1,8,27,64,125
+
+Console.WriteLine("Введите число "); 
+int Namber = Convert.ToInt32(Console.ReadLine()); 
+ 
+void Cube (int N) 
+{ 
+    for (int i = 1; i <= N; i++) 
+    { 
+        int Namber2 = i * i * i; 
+        Console.Write($"{Namber2},"); 
+    } 
+} 
+Cube(Namber);
